51aa60c747caa15471adb8ea59f757b227f3dc11
Ilham Akbar Ahmadi <ilham@amxware.com>
2022-04-05 15:56:54 +0700

    downgrade react version and jenkins script unitest

---
+++ src/tests/coba.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+
+describe('Async component', () => {
+  test('renders posts if request succeeds', async () => {
+    expect(true).toBe(true);
+  });
+});
