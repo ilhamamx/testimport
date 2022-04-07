168a7affc38dc345b842ba17c20bf82a21d96399
Ilham Akbar Ahmadi <ilham@amxware.com>
2022-04-07 15:33:13 +0700

    translation with ii18next library

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+
+  const { t, i18n } = useTranslation();
+
+  function changeLanguage(lng: string) {
+    i18n.changeLanguage(lng);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +26,10 @@ function App() {
         >
           Learn React
         </a>
+        <a>Ini contoh penggunaan translate</a>
+        <button onClick={() => changeLanguage('id')}>Change to id</button>
+        <button onClick={() => changeLanguage('en')}>Change to en</button>
+        <p>{t('Hello.World')}</p>
       </header>
     </div>
   );
