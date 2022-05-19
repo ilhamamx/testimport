e82ab983c079b84982c88ea204c2badaa07503ad
fikricghuv <fikricghuv@gmail.com>
2022-05-19 13:10:19 +0700

    add route contact, handled message, unhandled message

---
+++ 7days-client.js/src/app/pages/Contact.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { useTranslation } from "react-i18next";
+
+const Contact: FC = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <div className="fw-bold fs-3 text-gray-400 mb-15" data-testid="Contact-page"> 
+        <h2>Contact Page</h2>
+      </div>
+    </>
+  );
+};
+
+export { Contact };
