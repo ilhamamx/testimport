7c63845dcfc0bb66f8080e1fec62392bcb4e0842
fikricghuv <fikricghuv@gmail.com>
2022-05-24 10:37:38 +0700

    test push

--- 7days-client.js/src/app/pages/Contact.tsx
+++ 7days-client.js/src/app/pages/Contact.tsx
@@ -6,7 +6,7 @@ const Contact: FC = () => {
   return (
     <>
       <div className="fw-bold fs-3 text-gray-400 mb-15" data-testid="Contact-page"> 
-        <h2>Contact Page</h2>
+        <h2>Contact Page test</h2>
       </div>
     </>
   );
