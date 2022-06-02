968af816783c80f9fe631fe603451503553c3680
bryan adams <bryan@amxware.com>
2022-06-02 10:11:48 +0700

    erge

--- 7days-client.js/src/app/layout/MasterLayout.tsx
+++ 7days-client.js/src/app/layout/MasterLayout.tsx
@@ -36,6 +36,10 @@ const MasterLayout = () => {
   const isDesktopOrLaptop = useMediaQuery({
     query: "(min-width: 900px)",
   });
+<<<<<<< HEAD
+=======
+
+>>>>>>> 15b1766 (layout)
   const dispatch = useDispatch();
   const nav = useNavigate();
 
